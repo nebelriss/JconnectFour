3fcdf8521e93a0f1bf94c3c997eba1a197880140
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-19 22:50:47 +0100

    init FieldTest

---
+++ test/ch/fhnw/connectFour/persistance/FieldTest.java
@@ -0,0 +1,22 @@
+package ch.fhnw.connectFour.persistance;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FieldTest {
+
+	@Test
+	public void testField() {
+		Field field = new Field(2, 2);
+		
+		assertEquals("X is:", 2, field.getX());
+		assertEquals("Y is:", 2, field.getY());
+		
+		assertEquals("Owner:", FieldOwner.none, field.getFieldOwner());
+		
+		field.setFieldOwner(FieldOwner.cross);
+		assertEquals("Owner:", FieldOwner.cross, field.getFieldOwner());
+	}
+
+}
