43ebdf4ba055e19b9b29fe6e59285957b777befd
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-20 00:39:29 +0100

    some changes

--- src/ch/fhnw/connectFour/application/ApplicationProperties.java
+++ src/ch/fhnw/connectFour/application/ApplicationProperties.java
@@ -3,29 +3,41 @@ package ch.fhnw.connectFour.application;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ApplicationProperties {
 	
 	private static Logger log = Logger.getLogger("ch.fhnw.connectFour");
-
+	
+	private Properties defaultProperties;
+	private Properties applicationProperties;
 	
 	public ApplicationProperties() {
-		loadDefaultProps();
+		loadDefaultProperties();
+		loadApplicationProperties();
 		log.info("properties loaded.");
 	}
 	
-	private void loadDefaultProps() {
+	private void loadDefaultProperties() {
 		
-		Properties defaultProps = new Properties();
+		defaultProperties = new Properties();
 		try {
 			FileInputStream in = new FileInputStream("default.properties");
-			defaultProps.load(in);
+			defaultProperties.load(in);
 			in.close();
 		} catch (IOException e) {
 			log.warning(e.toString());
 		}
-		
+	}
+	
+	private void loadApplicationProperties() {
+		applicationProperties = new Properties(defaultProperties);
+		try {
+			FileInputStream in = new FileInputStream("application.properties");
+			applicationProperties.load(in);
+			in.close();
+		} catch (IOException e) {
+			log.warning(e.toString());
+		}
 	}
 }
