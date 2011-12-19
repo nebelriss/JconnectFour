d7d2939a02ce16bb2e08634c64c8c29796e78a42
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-19 22:57:39 +0100

    init Main Panel

---
+++ src/ch/fhnw/connectFour/gui/MainPanel.java
@@ -0,0 +1,17 @@
+package ch.fhnw.connectFour.gui;
+
+import javax.swing.JPanel;
+
+import ch.fhnw.connectFour.application.ApplicationContext;
+
+public class MainPanel extends JPanel{
+	
+	private ApplicationContext applicationContext;
+	
+	public MainPanel(ApplicationContext applicationContext) {
+		
+		this.applicationContext = applicationContext;
+		
+	}
+
+}
