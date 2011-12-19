fe5d5e87edcd2cf68c22f43726af43cf43103966
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-19 11:53:12 +0100

    init junit test of the main class

---
+++ test/ch/fhnw/connectFour/main/ConnectfourTest.java
@@ -0,0 +1,14 @@
+package ch.fhnw.connectFour.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConnectfourTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
