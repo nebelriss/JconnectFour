0d92967bb1194bb0717ad011bfc2802cb08c4c76
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-19 19:55:40 +0100

    init Application Logger

---
+++ src/ch/fhnw/connectFour/application/ApplicationLogger.java
@@ -0,0 +1,15 @@
+package ch.fhnw.connectFour.application;
+
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+public class ApplicationLogger {
+	
+	private static Logger logger = Logger.getLogger("ch.fhnw.connectFour");
+	private static FileHandler fh;
+	
+	public ApplicationLogger() {
+		
+	}
+
+}
