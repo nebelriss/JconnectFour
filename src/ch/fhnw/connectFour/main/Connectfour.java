bdd87cb2348e92ed4d15e2e137a7275bc48c8bcb
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-19 11:11:47 +0100

    init

---
+++ src/ch/fhnw/connectFour/main/Connectfour.java
@@ -0,0 +1,13 @@
+package ch.fhnw.connectFour.main;
+
+public class Connectfour {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
