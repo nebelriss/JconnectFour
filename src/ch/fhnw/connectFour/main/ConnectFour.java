fd6c2d51c3b0c282ae0f6ffa56be3484c5025061
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-19 22:52:00 +0100

    del unused imports

--- src/ch/fhnw/connectFour/main/ConnectFour.java
+++ src/ch/fhnw/connectFour/main/ConnectFour.java
@@ -3,17 +3,10 @@ package ch.fhnw.connectFour.main;
 import ch.fhnw.connectFour.application.ConnectFourInit;
 
 public class ConnectFour {
-	
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		
 		new ConnectFourInit();
-
+		
 	}
-	
-
-
 }
