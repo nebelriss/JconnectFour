90fbf6a2e096f253f646858f7fb83a21229eba6f
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-20 20:33:16 +0100

    init impl of fieldModel

---
+++ src/ch/fhnw/connectFour/logic/impl/FieldModelImpl.java
@@ -0,0 +1,27 @@
+package ch.fhnw.connectFour.logic.impl;
+
+import ch.fhnw.connectFour.logic.FieldListener;
+import ch.fhnw.connectFour.logic.FieldModel;
+import ch.fhnw.connectFour.persistance.FieldOwner;
+
+public class FieldModelImpl implements FieldModel {
+
+	@Override
+	public void setFieldChanged(int x, int y, FieldOwner fieldOwner) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public FieldOwner getFieldOwner(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addListener(FieldListener fieldListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
