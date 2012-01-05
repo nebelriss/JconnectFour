d8ffd168d698d500bdbaf715f39fbf83ba42db94
Michel Heiniger <michel.heiniger@gmail.com>
2012-01-06 00:30:22 +0100

    init game controller

---
+++ src/ch/fhnw/connectFour/logic/GameController.java
@@ -0,0 +1,22 @@
+package ch.fhnw.connectFour.logic;
+
+import java.util.logging.Logger;
+
+import ch.fhnw.connectFour.application.ApplicationContext;
+
+public class GameController {
+	
+	private static Logger log = Logger.getLogger("ch.fhnw.connectFour");
+
+	
+	public GameController(ApplicationContext applicationContext) {
+		
+		// player begins
+		
+	}
+	
+	public boolean isHumanPlayer() {
+		return true;
+	}
+
+}
