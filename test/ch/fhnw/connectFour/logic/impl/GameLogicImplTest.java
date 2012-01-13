734414181ce2663027bbe0fbe91c2cc8f03e55f6
Michel Heiniger <michel.heiniger@gmail.com>
2012-01-13 13:19:56 +0100

    init test for the alpha beta algorithm

---
+++ test/ch/fhnw/connectFour/logic/impl/GameLogicImplTest.java
@@ -0,0 +1,14 @@
+package ch.fhnw.connectFour.logic.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameLogicImplTest {
+
+	@Test
+	public void testGetNextMove() {
+		fail("Not yet implemented");
+	}
+
+}
